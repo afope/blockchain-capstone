bdb378227d3e1f3bfb2f0bbc5606d8a8d3060edb
afope <afopefoluwa@unchain.io>
2020-08-21 18:05:39 +0200

    feature: deploy contracts

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,11 +1,15 @@
 // migrating the appropriate contracts
-// var Verifier = artifacts.require("./verifier.sol");
-// var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
-// var ERC721MintableComplete = artifacts.require("ERC721MintableComplete");
+var Verifier = artifacts.require("./verifier.sol");
+var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 
-module.exports = function(deployer) {
-  // deployer.deploy(Verifier);
-  // deployer.deploy(SolnSquareVerifier);
-    // deployer.deploy(ERC721MintableComplete);
+module.exports = function (deployer) {
+  deployer.deploy(Verifier).then(() => {
+    return deployer.deploy(
+      SolnSquareVerifier,
+      Verifier.address,
+      "token MINT",
+      "TMT"
+    );
+  });
 };
